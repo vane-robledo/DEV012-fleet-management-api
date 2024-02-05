43c89070861838409b3d282a5d510e5cee61f5fd
vanelopezr <vanne.lopez.r@gmail.com>
2024-02-05 15:29:30 -0300

    controlador de trayectorias aparte

---
+++ api/src/main/java/com/fleetmanagment/api/TrajectoriesController.java
@@ -0,0 +1,23 @@
+package com.fleetmanagment.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class TrajectoriesController {
+    @Autowired
+    private TrajectoriesService trajectoriesService;
+
+    @GetMapping("/trajectories")
+
+    public List<Trajectories> findByTaxi(@PageableDefault(page = 0, size = 20) Pageable pageable) {
+        return trajectoriesService.getAllTrajectories(pageable);
+    }
+}
